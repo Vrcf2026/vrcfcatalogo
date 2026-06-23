a765254145845f68ba23342f48595a8fabe711ff
Vrcf2026 <vrcfrancisco@gmail.com>
2026-06-23 12:57:29 +0100

    Update WorldCatalog.tsx

--- src/pages/WorldCatalog.tsx
+++ src/pages/WorldCatalog.tsx
@@ -476,7 +476,7 @@ const WorldCatalog = ({ mundo, title, subtitle }: Props) => {
 
       {/* Banners deste mundo (ou transversais, mundo="todos") */}
       {banners.length > 0 && (
-        <div className="relative overflow-hidden bg-black min-h-[110px]" style={{ aspectRatio: "1200 / 300" }}>
+        <div className="relative overflow-hidden bg-black min-h-[110px]" style={{ aspectRatio: "1200 / 300", maxHeight: "clamp(220px, 28vw, 340px)" }}>
           {banners.map((b: any, i: number) => (
             <div key={b.id} className={`absolute inset-0 transition-opacity duration-500 ${i === bannerIdx ? "opacity-100" : "opacity-0"}`}>
               {b.link
