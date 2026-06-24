9065fd222a7ba30dd4abc8bf024eeeae40caa105
Vrcf2026 <vrcfrancisco@gmail.com>
2026-06-24 14:44:51 +0100

    Update QuantitySelector.tsx

--- src/components/QuantitySelector.tsx
+++ src/components/QuantitySelector.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { Minus, Plus, ShoppingCart, X } from "lucide-react";
 import { Button } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
 
 interface QuantitySelectorProps {
   /** Quantidade mínima (MOQ). Default: 1. */
